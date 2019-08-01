e9b68e4ab2e270e86d6e72d986d7f04d996eb573
tdnicola <tdnicola@gmail.com>
2019-08-01 13:29:20 -0400

    added server-auth and serverless

---
+++ auth-server/handler.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const axios = require('axios');
+
+module.exports.getAccessToken = async (event) => {
+
+    const MEETUP_OAUTH_URL = 'https://secure.meetup.com/oauth2/access'
+      + '?client_id=337jbe8sopjh7q62t45t732omi'
+      + '&client_secret=q1atlc62b28m8eek3p1fvvh8a9'
+      + '&grant_type=authorization_code'
+      + '&redirect_uri=https://tdnicola.github.io/meetup/'
+      + '&code=' + event.pathParameters.code;
+
+    const info = await axios.post(MEETUP_OAUTH_URL);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        access_token: info.data.access_token,
+        refresh_token: info.data.refresh_token,
+      }),
+    };
+};
