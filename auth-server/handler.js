1f00665069fb33862d7267f06f879b4db9eab794
tdnicola <tdnicola@gmail.com>
2019-08-01 21:48:14 -0400

    added refresh token function

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -9,15 +9,40 @@ module.exports.getAccessToken = async (event) => {
       + '&client_secret=q1atlc62b28m8eek3p1fvvh8a9'
       + '&grant_type=authorization_code'
       + '&redirect_uri=https://tdnicola.github.io/meetup/'
-      + '&code=' + event.pathParameters.code;
+      + '&code=2d5c274943f43641f23a10f061a0c025';
 
     const info = await axios.post(MEETUP_OAUTH_URL);
 
     return {
       statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
       body: JSON.stringify({
         access_token: info.data.access_token,
         refresh_token: info.data.refresh_token,
       }),
     };
 };
+
+module.exports.refreshAccessToken = async (event) => {
+
+  const MEETUP_OAUTH_URL = 'https://secure.meetup.com/oauth2/access'
+    + 'client_id=337jbe8sopjh7q62t45t732omi'
+    + '&client_secret=q1atlc62b28m8eek3p1fvvh8a9'
+    + '&grant_type=refresh_token'
+    + '&refresh_token=' + event.pathParameters.code;
+
+  const info = await axios.post(MEETUP_OAUTH_URL)
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      access_token: info.data.access_token,
+      refresh_token: info.data.refresh_token,
+    }),
+  };
+}
