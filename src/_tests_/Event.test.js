00589167589ea95f04650203b66c36f795ef445a
tdnicola <tdnicola@gmail.com>
2019-08-09 15:55:26 -0400

    changing event.test to have preloaded events and test to see if event.js correctly pulls data.

--- src/_tests_/Event.test.js
+++ src/_tests_/Event.test.js
@@ -26,8 +26,9 @@ describe('<Event /> component', () => {
     })
 
     // test('check to see if meetup date is correct', () => {
-    //     expect(EventWrapper.find('.eventDate')).toBe('2019-08-07')        
+    //     expect(EventWrapper.find('.eventDate').text()).toBe('2019-08-07');
     // })
+
     test('Check to see if button exists', () => {
       expect(EventWrapper.find('.eventButton')).toHaveLength(1);  
     })
@@ -37,5 +38,44 @@ describe('<Event /> component', () => {
         expect(EventWrapper.state('showDetails')).toBe(true);
     })
 
+    test('check to see if meetup date is correct', () => {
+        EventWrapper.setState({
+            events: [ 
+                {
+                    created: 1563366230000,
+                    duration: 3600000,
+                    id: "263222596",
+                    name: "1 Million Cups Augusta at theClubhou.se",
+                    date_in_series_pattern: false,
+                    status: "upcoming",
+                    time: 1565179200000,
+                    local_date: "2019-08-07",
+                    local_time: "08:00",
+                    updated: 1563366230000,
+                    utc_offset: -14400000,
+                    waitlist_count: 0,
+                    yes_rsvp_count: 21,
+
+                    venue: {
+                        id: 25900005,
+                        name: "Georgia Cyber Center Hull Mcknight Building",
+                        lat: 33.459999084472656,
+                        lon: -81.97000122070312,
+                        repinned: false,
+                        address_1: "1 11th St",
+                        city: "Augusta",
+                        country: "us",
+                        localized_country_name: "USA",
+                        zip: "30901",
+                        state: "GA"
+                    },
+                }
+            ]
+        });
+
+        expect(EventWrapper.find('.eventDate').text()).toBe('2019-08-07');
+    
+        
+    })
 
 })
