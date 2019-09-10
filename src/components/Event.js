6dc49c68ad333c3c403bbf2d54a555cda80df534
tdnicola <tdnicola@gmail.com>
2019-09-09 21:03:06 -0400

    correcting group name.

--- src/components/Event.js
+++ src/components/Event.js
@@ -32,7 +32,7 @@ class Event extends Component {
                 <div className='eventVenue'>Venue: {event.venue && event.venue.address_1}</div> 
                 <div className='eventStatus'>Status: {event.status}</div>  
                 <div className='eventStatus'>Name: {event.name}</div>
-                <div className='eventStatus'>Group: {event.group}</div>
+                <div className='eventStatus'>Group: {event.group.name}</div> 
 
 
                 {/* <div className='peopleGoing'>{event.rsvp_limit} people going.</div>   */}
