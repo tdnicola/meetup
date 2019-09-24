77661381d6db2956625d14ecc873c92b2a85bd5a
tdnicola <tdnicola@gmail.com>
2019-09-23 22:35:27 -0400

    furthering bdd testing for CitySearch.

--- src/_tests_/features/filterEventsByCity.test.js
+++ src/_tests_/features/filterEventsByCity.test.js
@@ -1,4 +1,9 @@
+import React from 'react';
 import { loadFeature, defineFeature } from 'jest-cucumber';
+import { mount, shallow } from 'enzyme';
+import { mockData } from '../../mockData/mockData'
+import App from '../../App';
+import CitySearch from '../../components/CitySearch';
 
 const feature = loadFeature('./src/_tests_/features/filterEventsByCity.feature');
 
@@ -8,47 +13,57 @@ defineFeature(feature, test => {
 
     	});
 
+			let AppWrapper;
     	when('the user opens the app', () => {
-
+			AppWrapper = mount(<App />);
     	});
 
     	then('the user should see the list of upcoming events from their location', () => {
-
+			AppWrapper.update();
+			expect(AppWrapper.find('.event')).toHaveLength(mockData.events.length);
     	});
     });
 
     test('User should see a list of suggestions when they search for a city', ({ given, when, then }) => {
-    	given('the main page is open', () => {
+		let CitySearchWrapper;
 
+    	given('the main page is open', () => {
+			CitySearchWrapper = shallow(<CitySearch />)
     	});
 
     	when('user starts typing in the city textbox', () => {
-
+			CitySearchWrapper.find('.city').simulate('change', { target: { value: 'Denver' }})
     	});
 
     	then('the user should receive a list of cities (suggestions) that match what they’ve typed', () => {
-
+			expect(CitySearchWrapper.find('.suggestions')).toHaveLength(1);
     	});
     });
 
     test('User can select a city from the suggested list', ({ given, and, when, then }) => {
-    	given('user was typing “Denver” in the city textbox', () => {
+		let AppWrapper;
 
+    	given('user was typing “Denver” in the city textbox', () => {
+			AppWrapper = mount(<App />);
+			AppWrapper.find('.city').simulate('change', { target: { value: 'Denver' }})
     	});
 
     	and('the list of suggested cities is showing', () => {
-
+			AppWrapper.update();
+			expect(AppWrapper.find('.suggestions li')).toHaveLength(3);
     	});
 
     	when('the user selects a city from the list', () => {
-
+			AppWrapper.find('.suggestions li').at(0).simulate('click');
     	});
 
     	then('their city should be changed to that city', () => {
-
+			const CitySearchWrapper = AppWrapper.find(CitySearch);
+			expect(CitySearchWrapper.state('query')).toBe('Denver, Colorado, USA');
     	});
 
     	and('the user should receive a list of upcoming events in that city', () => {
+			expect(AppWrapper.find('.event')).toHaveLength(mockData.events.length);
 
     	});
     });
